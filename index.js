9fd7891367c0abe3e8d15bdd81258f9aebc80dc0
bennu <benarzee1212@gmail.com>
2021-10-12 19:11:16 +0530

    new

---
+++ index.js
@@ -0,0 +1,41 @@
+var num1=20
+var num2=30
+var num3=40
+
+// = operator
+num1=10
+console.log(num1)
+
+// += operator
+num1 += 11 //adds 11 to the above 10 from line 6 num1 =num1+11
+console.log(num1)
+
+// // -= operator
+num1 -= 5 //subtracts 5 from the above 11 from line 10 num1=num1-5
+console.log(num1)
+
+//*= operator
+num1 *= 5
+console.log(num1) //num1 = num1*5
+
+// /= operator
+num1 /= 5
+console.log(num1) //num1 = num1/5
+
+//%= operator
+num1 %=5
+console.log(num1) //num1=num1%5 (remainder)
+
+//other operators
+
+//<<=	   x <<= y    	x = x << y
+// >>=	   x >>= y   	x = x >> y
+// >>>=	   x >>>= y 	x = x >>> y
+// &=	   x &= y	    x = x & y
+// ^=	   x ^= y	    x = x ^ y
+// |=	   x |= y	    x = x | y
+// **=	   x **= y  	x = x ** y
+
+
+
+
